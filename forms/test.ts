081e0ed151cd03207e0493872d49e770d07f10bb
noarsoft <popthebreaker@hotmail.com>
2024-11-30 17:44:48 +0700

    Create test.ts
    event

---
+++ forms/test.ts
@@ -0,0 +1,8 @@
+const eventHandlers = {
+    button1_onclick: () => {
+        console.log('Submit button clicked!');
+        alert('Submit button clicked!');
+    }
+};
+
+export default eventHandlers;
