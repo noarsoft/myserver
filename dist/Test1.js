0324149cc9745606f208f7fda7257ccc915edf43
noarsoft <popthebreaker@hotmail.com>
2025-09-22 14:36:13 +0700

    2 tests v4

--- dist/Test1.js
+++ dist/Test1.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const { utimes } = require("fs");
 const Utils_1 = require("./Utils");
 const unit_test = () => {
     //test case 1
@@ -17,3 +18,5 @@ const unit_test = () => {
         return;
     }
 };
+
+unit_test();
