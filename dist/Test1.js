ec2699af1a18eaa12465b156c8981f2ae0f58152
noarsoft <popthebreaker@hotmail.com>
2025-09-22 14:23:29 +0700

    ok

--- dist/Test1.js
+++ dist/Test1.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const Utils_1 = require("./Utils");
-if ((0, Utils_1.add)(2, 3) === 4) {
+if ((0, Utils_1.add)(2, 3) === 5) {
     console.log(0);
 }
 else {
