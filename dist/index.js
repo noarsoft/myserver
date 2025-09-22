38ed65f86a5148836771b309b57527994c8a7091
noarsoft <popthebreaker@hotmail.com>
2025-09-22 14:02:20 +0700

    Test1 run

--- dist/index.js
+++ dist/index.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const Express = require("express");
 const app = Express();
 app.get("/", (req, res) => {
-    res.send("Hello, World!1212345");
+    res.send("Hello, World!123");
 });
 app.listen(3000, () => {
     console.log("Server is running on http://localhost:3000");
