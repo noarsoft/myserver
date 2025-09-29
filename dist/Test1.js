16518ac396c01cc3d24e33c3a767d8faa645dcc7
noarsoft <popthebreaker@hotmail.com>
2025-09-29 14:33:46 +0700

    for deploy

--- dist/Test1.js
+++ dist/Test1.js
@@ -1,7 +1,16 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const Utils_1 = require("./Utils");
-const unit_test = () => {
+const unit_test = () => __awaiter(void 0, void 0, void 0, function* () {
     //test case 1
     if ((0, Utils_1.add)(2, 3) === 5) {
     }
@@ -10,11 +19,11 @@ const unit_test = () => {
         return;
     }
     //test case 2
-    if ((0, Utils_1.add)(-2, -3) === -1) {
+    if ((0, Utils_1.add)(-2, -3) === -5) {
     }
     else {
         console.log(2);
         return;
     }
-};
+});
 unit_test();
--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,28 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const Express = require("express");
+const mongoose_1 = __importDefault(require("mongoose"));
+const UserRoutes_1 = __importDefault(require("./UserRoutes"));
+const cors_1 = __importDefault(require("cors"));
+const path = require("path");
 const app = Express();
+const mongoURI = "mongodb+srv://iampop242_db_user:test1234@cluster0.fbcapr5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+mongoose_1.default
+    .connect(mongoURI)
+    .then(() => {
+    console.log("MongoDB connected");
+})
+    .catch((error) => {
+    console.error("MongoDB connection error:", error);
+});
+app.use(Express.json());
+app.use((0, cors_1.default)());
+app.use(Express.static(path.join(__dirname, "public")));
+// Routes
+app.use("/api", UserRoutes_1.default);
 app.get("/", (req, res) => {
     res.send("Hello, World!123");
 });
